ecef81d3da4618064d82269c090651ab6887e35a
Marianela4 <141262093+Marianela4@users.noreply.github.com>
2026-07-30 14:02:27 -0300

    Add files via upload

---
+++ scratch-tarea-web/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='detective-scratch-v4';
+const FILES=['./','index.html','styles.css','script.js','manifest.webmanifest','icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
